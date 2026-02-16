117913ec113cd656bc40222e4a671fc37ac3b1b8
sheikmeera <160885234+sheikmeera@users.noreply.github.com>
2026-02-16 16:15:01 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.js
@@ -0,0 +1,29 @@
+// Last updated: 16/02/2026, 16:15:01
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let n = height.length;
+    if (n === 0) return 0;
+
+    let leftMax = new Array(n);
+    let rightMax = new Array(n);
+    let water = 0;
+
+    leftMax[0] = height[0];
+    for (let i = 1; i < n; i++) {
+        leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+    }
+
+    rightMax[n - 1] = height[n - 1];
+    for (let i = n - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+    }
+
+    for (let i = 0; i < n; i++) {
+        water += Math.min(leftMax[i], rightMax[i]) - height[i];
+    }
+
+    return water;
+};
