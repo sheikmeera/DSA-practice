39445e2ddc7e4c1bfd3534298c60c70e9ecb8cbb
sheikmeera <160885234+sheikmeera@users.noreply.github.com>
2026-02-16 16:13:51 +0530

    Create 2864-IsObjectEmpty

---
+++ 2864-IsObjectEmpty/2864-IsObjectEmpty.js
@@ -0,0 +1,8 @@
+// Last updated: 16/02/2026, 16:13:50
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return Object.keys(obj).length===0?true:false
+};
