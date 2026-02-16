3a339fa341e688561a1c10e6f1b3713ef0e1a840
sheikmeera <160885234+sheikmeera@users.noreply.github.com>
2026-02-16 16:14:06 +0530

    Create 2796-AllowOneFunctionCall

---
+++ 2796-AllowOneFunctionCall/2796-AllowOneFunctionCall.js
@@ -0,0 +1,24 @@
+// Last updated: 16/02/2026, 16:14:06
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called=false
+    return function(...args){
+        if(!called){
+            called=true
+            return fn(...args)
+            
+        }
+        return undefined;
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
