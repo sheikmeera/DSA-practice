ea4c295cd4dc500d7ec9161d15a6c317e4ab3017
sheikmeera <160885234+sheikmeera@users.noreply.github.com>
2026-02-16 16:14:02 +0530

    Create 2813-ToBeOrNotToBe

---
+++ 2813-ToBeOrNotToBe/2813-ToBeOrNotToBe.js
@@ -0,0 +1,31 @@
+// Last updated: 16/02/2026, 16:14:02
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe:function(value){
+        if(val===value){
+            return true;
+        }
+        else{
+            throw new Error("Not Equal")
+        }
+    },
+        notToBe:function(value){
+        if(val!==value){
+            return true;
+           
+        }
+        else{
+             throw new Error("Equal")
+        }
+    }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
