af85bd5cf80304df652376e88b9ecb39e7c1f15a
sheikmeera <160885234+sheikmeera@users.noreply.github.com>
2026-02-16 16:14:43 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.js
@@ -0,0 +1,22 @@
+// Last updated: 16/02/2026, 16:14:42
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverseBits = function(n) {
+    let bin=n.toString(2);
+    let val=bin.split("").reverse().join("")
+    let res=[];
+    let p=0;
+    for(let i=0;i<32;i++){
+        if(p<val.length){
+            res.push(val[p])
+            p++
+        }
+        else{
+            res.push(0)
+        }
+    }
+    val=parseInt(res.join(""),2)
+    return val
+};
