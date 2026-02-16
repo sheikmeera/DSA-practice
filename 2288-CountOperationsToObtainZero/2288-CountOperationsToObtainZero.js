821ef7a11abc10be1ab4e6b90549922dc54bedd1
sheikmeera <160885234+sheikmeera@users.noreply.github.com>
2026-02-16 16:14:22 +0530

    Create 2288-CountOperationsToObtainZero

---
+++ 2288-CountOperationsToObtainZero/2288-CountOperationsToObtainZero.js
@@ -0,0 +1,19 @@
+// Last updated: 16/02/2026, 16:14:21
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var countOperations = function(num1, num2) {
+    let count=0;
+    while(num1!==0&&num2!==0){
+        if(num1>=num2){
+            num1=num1-num2;
+        }
+        else{
+            num2=num2-num1;
+        }
+        count++;
+    }
+    return count;
+};
