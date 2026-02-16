3d5741b4008e3de5c214f9be01144e14ddfe04cc
sheikmeera <160885234+sheikmeera@users.noreply.github.com>
2026-02-16 16:15:05 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.js
@@ -0,0 +1,9 @@
+// Last updated: 16/02/2026, 16:15:04
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
