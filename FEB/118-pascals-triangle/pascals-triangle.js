3da90b4a8c95ac9862d0e4052e3397427d099ad8
N.Sheik Fareeth Valiullah <160885234+sheikmeera@users.noreply.github.com>
2026-03-16 09:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (73.24%) - LeetSync

---
+++ FEB/118-pascals-triangle/pascals-triangle.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const res=[];
+    for(let i=0;i<numRows;i++){
+        let row=new Array(i+1).fill(1);
+        for(let j=1;j<i;j++){
+            row[j]=res[i-1][j-1]+res[i-1][j];
+        }
+        res.push(row)
+    }
+    return res
+};
