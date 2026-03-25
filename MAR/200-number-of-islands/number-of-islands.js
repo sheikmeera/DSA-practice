db8509bef15793073486728d9ef4d0c2ac54c457
N.Sheik Fareeth Valiullah <160885234+sheikmeera@users.noreply.github.com>
2026-03-25 10:17:03 +0530

    Time: 49 ms (95.83%) | Memory: 59 MB (60.06%) - LeetSync

---
+++ MAR/200-number-of-islands/number-of-islands.js
@@ -0,0 +1,26 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let count=0;
+    function dfs(i,j){
+        if(i>=grid.length||i<0||j<0||j>=grid[0].length||grid[i][j]==0){
+            return;
+        }
+        grid[i][j]=0;
+        dfs(i+1,j)
+        dfs(i-1,j)
+        dfs(i,j+1)
+        dfs(i,j-1)
+    }
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]==1){
+                count++
+                dfs(i,j)
+            }
+        }
+    }
+    return count
+};
