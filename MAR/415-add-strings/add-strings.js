b442c50b107c3a8578a7b31ff4f689ec9a8ea5f1
N.Sheik Fareeth Valiullah <160885234+sheikmeera@users.noreply.github.com>
2026-07-21 12:34:15 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (73.94%) - LeetSync

--- MAR/415-add-strings/add-strings.js
+++ MAR/415-add-strings/add-strings.js
@@ -4,22 +4,8 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-    let i = num1.length - 1;
-    let j = num2.length - 1;
-    let carry = 0;
-    let result = [];
-
-    while (i >= 0 || j >= 0 || carry) {
-        let x = i >= 0 ? num1.charCodeAt(i) - 48 : 0;
-        let y = j >= 0 ? num2.charCodeAt(j) - 48 : 0;
-
-        let sum = x + y + carry;
-        result.push(sum % 10);
-        carry = Math.floor(sum / 10);
-
-        i--;
-        j--;
-    }
-
-    return result.reverse().join("");
+    let a=BigInt(num1)
+    let b=BigInt(num2);
+    let sum=a+b;
+    return sum.toString()
 };
