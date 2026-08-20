aedfa961c274b80eeaca3be64b29f49da9f9da16
N.Sheik Fareeth Valiullah <160885234+sheikmeera@users.noreply.github.com>
2026-08-20 11:24:10 +0530

    Time: 2 ms (68.42%) | Memory: 59.1 MB (45.61%) - LeetSync

---
+++ MAR/55-jump-game/jump-game.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let farthest=0;
+
+    for(let i=0;i<nums.length;i++){
+        if(i>farthest){
+            return false;
+        }
+        farthest=Math.max(farthest,i+nums[i])
+        if(farthest>=nums.length-1){
+            return true;
+        }
+    }
+};
